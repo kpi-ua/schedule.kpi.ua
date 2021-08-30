75aa0b70bc9166f6832458d882829a5459907b5d
Oleksii Novoseltsev <ono@rismasystems.com>
2021-08-30 10:36:57 +0300

    Code formatted

---
+++ src/components/scheduleItem/index.js
@@ -0,0 +1,32 @@
+import teacherIcon from '../../assets/icons/teacher.svg';
+import locationIcon from '../../assets/icons/location.svg';
+import {
+    Location,
+    ScheduleItemType,
+    Subject,
+    Teacher,
+} from './scheduleItem.style';
+
+
+export const ScheduleItem = ({ scheduleItemData, activeTab }) => {
+    return (
+        <ScheduleItemType>
+            <ScheduleItemType>
+                Лекція
+            </ScheduleItemType>
+            <Subject>
+                Вишмат
+            </Subject>
+            <Teacher>
+                <img src={teacherIcon} alt='teacher'/>
+                Викладач
+            </Teacher>
+            <Location>
+                <img src={locationIcon} alt='location'/>
+                -18
+            </Location>
+        </div>
+    )
+}
+
+export default ScheduleItem;
---
+++ src/containers/schedule/schedule.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const GridWrapper = styled.div`
+  background-color: rgba(255, 255, 255, 0.5);
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
+  border-radius: 40px;
+  padding: 48px 24px;
+  margin: 36px 48px 0;
+`
