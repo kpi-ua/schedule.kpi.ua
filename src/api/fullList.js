ee547c937107229c1b2beeb27692b52dee689bc4
Oleks-Y <borith1111@gmail.com>
2022-06-04 19:11:45 +0200

    update get groups request

--- src/api/fullList.js
+++ src/api/fullList.js
@@ -1,11 +1,15 @@
-import Http from './index';
+import Http from "./index";
 
 export const getAllLecturers = () => {
-  return Http.get('/schedule/lecturer/list')
-    .then(({data}) => data);
-}
+  return Http.get("/schedule/lecturer/list").then(({ data }) => data);
+};
 
 export const getAllGroups = () => {
-  return Http.get('/schedule/groups')
-    .then(({data}) => data);
-}
+  return Http.get("/schedule/groups").then(({ data }) => {
+    return {
+      data: data.data.map((row) => ({
+        name: `${row.name.trim()} (${row.faculty.trim()})`,
+      })),
+    };
+  }); // TODO concat group name with faculty name
+};
