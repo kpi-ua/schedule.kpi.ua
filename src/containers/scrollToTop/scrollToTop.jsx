7f3474b9f034d1bc5277850874b377666aeed8ef
Sergiy <56846383+s3rbug@users.noreply.github.com>
2022-07-14 02:20:50 +0300

    Set current day in mobile version (#98)

--- src/containers/scrollToTop/scrollToTop.jsx
+++ src/containers/scrollToTop/scrollToTop.jsx
@@ -5,7 +5,10 @@ import { withRouter } from 'react-router-dom';
 const ScrollToTop = ({ history, children }) => {
     useEffect(() => {
         const unlisten = history.listen(() => {
-            window.scrollTo(0, 0);
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
         });
         return () => {
             unlisten();
