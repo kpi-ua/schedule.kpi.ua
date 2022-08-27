c59fb59a25803737476552a6cfd10f6bb9cc1cef
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2022-08-28 00:19:25 +0300

    Fix withRouter issue
    Details: https://reactrouter.com/en/v6.3.0/faq

--- src/containers/scrollToTop/scrollToTop.jsx
+++ src/containers/scrollToTop/scrollToTop.jsx
@@ -1,5 +1,26 @@
 import { useEffect, Fragment } from 'react';
-import { withRouter } from 'react-router-dom';
+
+import {
+    useLocation,
+    useNavigate,
+    useParams,
+} from "react-router-dom";
+
+function withRouter(Component) {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let navigate = useNavigate();
+        let params = useParams();
+        return (
+            <Component
+                {...props}
+                router={{ location, navigate, params }}
+            />
+        );
+    }
+
+    return ComponentWithRouterProp;
+}
 
 // Scroll to top on location change
 const ScrollToTop = ({ history, children }) => {
