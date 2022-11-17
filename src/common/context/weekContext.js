ce72ecd4b01c3cd4eed5179f26a8e775a024590e
s3rbug <bugaychuk.sergiy@gmail.com>
2022-11-17 23:02:10 +0200

    Fixed default week

--- src/common/context/weekContext.js
+++ src/common/context/weekContext.js
@@ -1,11 +1,28 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
+import { useCurrentDateParams } from '../utils/useCurrentDateParams';
 
 const WeekContext = createContext(null);
 
 export const useWeekContext = () => useContext(WeekContext);
 
-export const WeekContextProvider = ({initialValue, children}) => {
-  const [currentWeek, setCurrentWeek] = useState(initialValue);
+export const WeekContextProvider = ({children}) => {
+  const [currentWeek, setCurrentWeek] = useState(null);
+  const currentDateParams = useCurrentDateParams();
+  const getWeekByNumber = (week) => {
+    if(week === 1){
+      return "firstWeek"
+    }
+    else if (week === 2){
+      return "secondWeek"
+    }
+    return null
+  }
+
+  useEffect(() => {
+    if(currentDateParams.currentWeek){
+      setCurrentWeek(getWeekByNumber(currentDateParams.currentWeek));
+    }
+  }, [currentDateParams.currentWeek])
 
   const params = {setCurrentWeek, currentWeek};
 
