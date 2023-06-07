0d377c0253cdbcd76f0f289428a6cd5f2169ffdd
David <electromandarin@gmail.com>
2023-06-08 00:06:40 +0300

    feat: corrected exam months

--- src/components/examComponent/exam.jsx
+++ src/components/examComponent/exam.jsx
@@ -1,44 +1,57 @@
-import { CardDate, CardMainData, CardWrapper, Divider, DividerRed, Location, Subject, Teacher, } from './exam.style';
-import teacherIcon from '../../assets/icons/teacher.svg';
-import locationIcon from '../../assets/icons/location.svg';
-import clock from '../../assets/icons/clock.svg';
-import { Flex, Pictogram } from '../../common/styles/styles';
+import {
+  CardDate,
+  CardMainData,
+  CardWrapper,
+  Divider,
+  DividerRed,
+  Location,
+  Subject,
+  Teacher,
+} from "./exam.style";
+import teacherIcon from "../../assets/icons/teacher.svg";
+import locationIcon from "../../assets/icons/location.svg";
+import clock from "../../assets/icons/clock.svg";
+import { Flex, Pictogram } from "../../common/styles/styles";
 
-const ExamComponent = ({data}) => {
-
+const ExamComponent = ({ data }) => {
   const subject = data && data.subject;
   const teacher = data && data.lecturerName;
   const location = data && data.room;
   const date = data && new Date(data.date);
   const daysLeft = data && data.daysLeft;
   const day = date.getDate();
+  const month = date.toLocaleString("uk-UA", { month: "long" });
+  const formattedMonth = month.charAt(0).toUpperCase() + month.slice(1).replace(/ень$/, "ня");
 
   return (
     <div>
       <CardWrapper>
         <Flex>
-          <DividerRed/>
+          <DividerRed />
           <CardMainData>
             <Subject>{subject}</Subject>
             <Teacher>
-              <Pictogram src={teacherIcon} alt="teacher"/>
+              <Pictogram src={teacherIcon} alt="teacher" />
               {teacher}
             </Teacher>
             <Teacher>
-              <Pictogram src={clock} alt="time"/>
-              {date.toLocaleTimeString('en-US', {timeStyle: 'short'})}
+              <Pictogram src={clock} alt="time" />
+              {date.toLocaleTimeString("en-US", { timeStyle: "short" })}
             </Teacher>
             <Location>
-              <Pictogram src={locationIcon} alt="location"/>
+              <Pictogram src={locationIcon} alt="location" />
               {location}
             </Location>
           </CardMainData>
         </Flex>
         <Flex>
-          <Divider/>
+          <Divider />
           <CardDate>
             <h3>{date.getFullYear()}</h3>
-            <h2> {day < 10 ? '0' + day : day} Січня</h2>
+            <h2>
+              {" "}
+              {day < 10 ? "0" + day : day} {formattedMonth}
+            </h2>
             {/* TODO calculate  */}
             <h3>{daysLeft} днів до початку</h3>
           </CardDate>
