36814f35a1595955843f7f2ab8e6e8e70366a5a0
David <electromandarin@gmail.com>
2023-06-08 01:23:28 +0300

    fix: February formatting fixed

--- src/containers/scheduleExams/scheduleExams.jsx
+++ src/containers/scheduleExams/scheduleExams.jsx
@@ -1,13 +1,13 @@
-import { GridWrapper, Header, WordAccent } from "./scheduleWrapper.style";
-import Exam from "../../components/examComponent";
-import { useGroupContext } from "../../common/context/groupContext";
-import { useWeekContext } from "../../common/context/weekContext";
-import { useEffect, useState } from "react";
+import { GridWrapper, Header, WordAccent } from './scheduleWrapper.style';
+import Exam from '../../components/examComponent';
+import { useGroupContext } from '../../common/context/groupContext';
+import { useWeekContext } from '../../common/context/weekContext';
+import { useEffect, useState } from 'react';
 
-const SchededuleExamsWrapper = ({ getData }) => {
+const SchededuleExamsWrapper = ({getData}) => {
   const [data, setData] = useState(null);
 
-  const { group } = useGroupContext();
+  const {group} = useGroupContext();
   const { currentWeek } = useWeekContext();
 
   useEffect(() => {
@@ -20,21 +20,20 @@ const SchededuleExamsWrapper = ({ getData }) => {
     }
   }, [group, getData]);
 
-  return data ? (
-    <div style={{ overflow: "hidden" }}>
-      <GridWrapper>
-        <Header>
-          Розклад сесії для групи <WordAccent>{group.name}</WordAccent>
-          <br />
-          на{" "}
-          <WordAccent>
-            {currentWeek === "firstWeek" ? "перший семестр" : "другий семестр"}
-          </WordAccent>
-        </Header>
-        {data && data.map((d, idx) => <Exam key={idx} data={d} />)}
-      </GridWrapper>
-    </div>
-  ) : null;
+  console.log(data);
+
+  return (
+    data ?
+      <div style={{overflow: 'hidden'}}>
+        <GridWrapper>
+          <Header>
+            Розклад сесії для групи <WordAccent>{group.name}</WordAccent><br/>
+            на <WordAccent>{currentWeek === "firstWeek" ? "перший семестр" : "другий семестр"}</WordAccent>
+          </Header>
+          {data && data.map((d, idx) => <Exam key={idx} data={d}/>)}
+        </GridWrapper>
+      </div> : null
+  );
 };
 
 export default SchededuleExamsWrapper;
