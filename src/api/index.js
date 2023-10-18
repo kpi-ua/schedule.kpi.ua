a386db0f086a1159e29343ba844b8f332b7f301a
Oleks-Y <borith1111@gmail.com>
2023-10-18 13:29:44 +0200

    feat: update api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 let headers = {};
 
-export const Http = axios.create(
-  {
-    baseURL: "https://schedule.kpi.ua/api/",
-    headers
-  });
-
+export const Http = axios.create({
+  baseURL: "https://api.campus.kpi.ua/",
+  headers,
+});
 
 export default Http;
