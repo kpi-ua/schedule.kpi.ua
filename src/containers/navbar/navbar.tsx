31f3734a55509e4040fa8b060e4f401f0f9b414a
Andrew Izvarin <andrew.izvarin@gmail.com>
2024-08-14 19:11:36 +0300

    refactor: theme switch and theme context

--- src/containers/navbar/navbar.tsx
+++ src/containers/navbar/navbar.tsx
@@ -1,19 +1,19 @@
 import { Logo } from '../../common/styles/styles';
 import MainSettings from '../mainSettings';
 import { NavbarInner, NavBarWrapper } from './navbar.style';
-import Switch from '../../components/switch';
+import ThemeSwitch from '../../components/themeSwitch';
 import logo from '../../assets/logo.svg';
 import { useThemeSelectorContext } from '../../common/context/themeContext';
 
 const Navbar = () => {
-  const selectTheme = useThemeSelectorContext();
+  const { currentTheme, changeTheme } = useThemeSelectorContext();
 
   return (
     <NavBarWrapper>
       <NavbarInner justifyContent="space-between" mx={2} gap={'20px'}>
         <Logo src={logo} alt={'logo'}/>
         <MainSettings/>
-        <Switch onChange={selectTheme.changeTheme}/>
+        <ThemeSwitch value={currentTheme} onChange={changeTheme}/>
       </NavbarInner>
     </NavBarWrapper>
   );
