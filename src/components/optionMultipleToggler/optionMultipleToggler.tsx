0ab30ec8fcb154423360ece79fd7cc8b4b78155f
Andrew Izvarin <andrew.izvarin@gmail.com>
2024-08-14 20:35:22 +0300

    refactor: week and semester switch

--- src/components/optionMultipleToggler/optionMultipleToggler.tsx
+++ src/components/optionMultipleToggler/optionMultipleToggler.tsx
@@ -1,49 +1,28 @@
+import { ListOption } from '../../types/ListOption';
 import { TogglerOption, TogglerWrapper } from './optionMultipleToggler.style';
-import React, { useEffect, useState } from 'react';
 
-interface Props {
-  options: {value: string, label: string}[],
-  onOptionChange: (value: string | null) => void,
-  initialValue: string | null,
+interface Props<T> {
+  options: ListOption<T>[];
+  onChange: (value: T) => void;
+  currentValue: T;
 }
 
-const OptionMultipleToggler: React.FC<Props> = ({options, onOptionChange, initialValue}) => {
-  const [selectedItem, selectItem] = useState(initialValue);
-
-  useEffect(() => {
-    selectItem(initialValue);
-
-    return () => onOptionChange(null);
-  }, [initialValue]);
-
-  useEffect(() => {
-    onOptionChange(selectedItem);
-  }, [selectedItem]);
-
-  const optionChangeHandler = (value: string) => () => {
-    selectItem(state => {
-      if (state !== value) {
-        return value;
-      }
-
-      return state;
-    });
-
+const OptionMultipleToggler = <T extends string | number>({ options, onChange, currentValue }: Props<T>) => {
+  const optionChangeHandler = (value: T) => () => {
+    onChange(value);
   };
-
-  const togglerOptions = (options || []).map(item => {
-    return (
-      <TogglerOption key={item.value}
-                     active={selectedItem === item.value}
-                     onClick={optionChangeHandler(item.value)}>
-        {item.label}
-      </TogglerOption>
-    );
-  });
 
   return (
     <TogglerWrapper>
-      {togglerOptions}
+      {options.map(item => (
+        <TogglerOption
+          key={item.value}
+          active={currentValue === item.value}
+          onClick={optionChangeHandler(item.value)}
+        >
+          {item.label}
+        </TogglerOption>
+      ))}
     </TogglerWrapper>
   );
 };
--- src/components/weekSwitch/weekSwitch.tsx
+++ src/components/weekSwitch/weekSwitch.tsx
@@ -1,5 +1,7 @@
 import OptionMultipleToggler from "../../components/optionMultipleToggler";
 import { useWeekContext } from "../../common/context/weekContext";
+import { Week } from '../../types/Week';
+import { ListOption } from '../../types/ListOption';
 
 interface WeekSwitchProps {
   type: "weeks" | "semesters";
@@ -8,26 +10,27 @@ interface WeekSwitchProps {
 const WeekSwitch = ({ type }: WeekSwitchProps) => {
   const { setCurrentWeek, currentWeek } = useWeekContext()!;
 
-  const updateWeek = (value: string | null) => {
+  const updateWeek = (value: Week) => {
     if (value) {
       setCurrentWeek(value);
     }
   };
 
-  const weeks = [
+  const weeks: ListOption<Week>[] = [
     { label: "Перший тиждень", value: "firstWeek" },
     { label: "Другий тиждень", value: "secondWeek" },
   ];
 
-  const semesters = [
+  const semesters: ListOption<Week>[] = [
     { label: "Перший семестр", value: "firstWeek" },
     { label: "Другий семестр", value: "secondWeek" },
   ];
+
   return (
     <OptionMultipleToggler
-      initialValue={currentWeek}
+      currentValue={currentWeek}
       options={type === "weeks" ? weeks : semesters}
-      onOptionChange={updateWeek}
+      onChange={updateWeek}
     />
   );
 };
