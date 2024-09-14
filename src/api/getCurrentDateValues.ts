71e8421cdcc4d5c5d71b8693b749eb26ddf1552d
Oleksii Hlavatskyi <qqlexa@flyaps.com>
2024-09-14 23:03:23 +0300

    Change to one-line expression

--- src/api/getCurrentDateValues.ts
+++ src/api/getCurrentDateValues.ts
@@ -3,6 +3,5 @@ import { PagedResponse } from '../models/PagedResponse';
 import Http from './index';
 
 export const getCurrentDateValues = async (): Promise<PagedResponse<CurrentTime>> => {
-  let res = await Http.get('/time/current');
-  return await res;
+  return await Http.get('/time/current');
 };
