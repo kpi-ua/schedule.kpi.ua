2d533e49a3c8d330fceae3578d5967d9e9c3b638
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-04-10 15:54:30 +0300

    remove redundant await

--- src/common/hooks/usePreloadedList.ts
+++ src/common/hooks/usePreloadedList.ts
@@ -6,15 +6,15 @@ import { Group } from '../../models/Group';
 export const usePreloadedList = () => {
   const { data: groups = [], isLoading: groupsLoading } = useQuery<Group[]>({
     queryKey: 'groups',
-    queryFn: async () => {
-      return  await getAllGroups();
+    queryFn: () => {
+      return getAllGroups();
     },
   });
 
   const { data: lecturers = [], isLoading: lecturersLoading } = useQuery<Lecturer[]>({
     queryKey: 'lecturers',
-    queryFn: async () => {
-      return  await getAllLecturers();
+    queryFn: () => {
+      return getAllLecturers();
     },
   });
 
