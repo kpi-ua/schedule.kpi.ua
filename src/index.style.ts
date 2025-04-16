675c2d38a8c18c8f134931f015fabf7758a74b69
Dmytro Marchuk <dmytro.marchuk1@lll.kpi.ua>
2025-04-16 21:54:37 +0300

    font changed to Inter

--- src/index.style.ts
+++ src/index.style.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
   * {
       box-sizing: border-box;
-      font-family: 'Exo 2', sans-serif;
+      font-family: 'Inter', sans-serif;
   }
   
   html {
