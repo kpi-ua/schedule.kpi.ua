34c270fe0b6ae00691f928c74c2d8564bb4a2e16
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-10-21 17:57:27 +0300

    Simplify date filtering functions

--- src/common/utils/dateFilters.ts
+++ src/common/utils/dateFilters.ts
@@ -1,14 +1,20 @@
 import dayjs from 'dayjs';
 
 /**
+ * Checks if a date is today or in the future
+ */
+const isDateTodayOrFuture = (date: string): boolean =>
+  dayjs().isBefore(date, 'date') || dayjs().isSame(date, 'date');
+
+/**
  * Filters dates to include only today and future dates
  */
 export const filterFutureDates = (dates: string[]): string[] =>
-  dates.filter((date) => dayjs().isBefore(date, 'date') || dayjs().isSame(date, 'date'));
+  dates.filter(isDateTodayOrFuture);
 
 /**
  * Checks if an array of dates contains at least one future or current date
  */
 export const hasFutureDates = (dates: string[]): boolean =>
-  dates.some((date) => dayjs().isBefore(date, 'date') || dayjs().isSame(date, 'date'));
+  dates.some(isDateTodayOrFuture);
 
