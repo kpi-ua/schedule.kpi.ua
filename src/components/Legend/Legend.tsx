7f1e871e37d89d3ca72d1ad9de30a4942d2758f5
Markusplay <mshilo2004@gmail.com>
2026-07-21 00:09:57 +0300

    migrate Legend component to tailwind

--- src/components/Legend/Legend.tsx
+++ src/components/Legend/Legend.tsx
@@ -1,59 +1,18 @@
-import styled from 'styled-components';
 import LastSyncDate from '../LastSyncDate';
 import { LessonsCount, SubjectTypeBadge } from '../SubjectTypeBadge/SubjectTypeBadge';
 import { SubjectType } from '../../models/Pair';
 import { SUBJECT_TYPES } from '../../common/constants/subjectTypes';
-import { getValueFromTheme } from '../../common/utils/getValueFromTheme';
 import dayjs from 'dayjs';
-
-const LegendWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 12px;
-  gap: 16px;
-`;
-
-const LegendHeaderWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const LegendHeader = styled.div`
-  font-size: 16px;
-  font-weight: 600;
-`;
-
-const LegendContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-`;
-
-const LegendItem = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  gap: 8px;
-`;
-
-const LessonsCountStyleProvider = styled.div`
-  --accent-color: ${getValueFromTheme('otherPurple')};
-
-  & > span {
-    padding-top: 2px;
-    padding-bottom: 2px;
-  }
-`;
 
 export const Legend = () => {
   return (
-    <LegendWrapper>
-      <LegendHeaderWrapper>
-        <LegendHeader>Легенда</LegendHeader>
+    <div className="mt-3 flex flex-col gap-4">
+      <div className="flex justify-between">
+        <div className="text-base font-semibold">Легенда</div>
         <LastSyncDate />
-      </LegendHeaderWrapper>
-      <LegendContent>
-        <LegendItem>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-start gap-2">
           <SubjectTypeBadge
             type={SubjectType.Lecture}
             // To always keep same value in badge
@@ -62,14 +21,14 @@ export const Legend = () => {
             {SUBJECT_TYPES.lec}
           </SubjectTypeBadge>
           <span>Непостійний тип заняття</span>
-        </LegendItem>
-        <LegendItem>
-          <LessonsCountStyleProvider>
+        </div>
+        <div className="flex items-center justify-start gap-2">
+          <div className="[--accent-color:#949dff] [&>span]:py-0.5">
             <LessonsCount>1/3</LessonsCount>
-          </LessonsCountStyleProvider>
+          </div>
           <span>Серія непостійних занять</span>
-        </LegendItem>
-      </LegendContent>
-    </LegendWrapper>
+        </div>
+      </div>
+    </div>
   );
 };
